b4a51d064d9e3d5d96f5b5146bfff445f336fabb
Ciaran Kamp <ciarankamp@gmail.com>
2018-03-23 10:09:09 +0000

    Core setup

---
+++ app/views/pages/index.php
@@ -0,0 +1,4 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<h1><?php echo $data['title']?></h1>
+<p>This is the CKMVC PHP framework please refer to the docs on how to use</p>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
